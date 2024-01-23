ad75513a85f1154d063852507e6b8ba9e55cbe53
Delt a rashadjamal <rashadjamalansari@gmail.com>
2024-01-23 18:56:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Program from './components/program/Program';
 function App() {
   return (
     <div className="App">
        <Hero/>
+       <Program/>
     </div>
   );
 }
